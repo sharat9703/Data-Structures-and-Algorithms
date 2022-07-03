eab02d65f384ee2d8bbac81c953b3052809695cf
Sharat Yaragatti <sharath.yaragatti@yahoo.com>
2022-07-03 23:47:02 +0530

    added the problem statement on comment

--- Arrays/MaxSubSumKConcaten.java
+++ Arrays/MaxSubSumKConcaten.java
@@ -1,5 +1,11 @@
-import java.util.ArrayList;
 
+/*Problem statement:
+You have been given a vector/list 'ARR' consisting of ‘N’ integers. You are also given a positive integer ‘K’.
+Let’s define a vector/list 'CONCAT' of size 'N * K' formed by concatenating 'ARR' ‘K’ times. For example, if 'ARR' = [0, -1, 2] and 'K' = 3, then 'CONCAT' is given by [0, -1, 2, 0, -1, 2, 0, -1, 2].
+Your task is to find the maximum possible sum of any non-empty subarray (contagious) of 'CONCAT'.
+*/
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.Integer;
 import java.lang.Math;
